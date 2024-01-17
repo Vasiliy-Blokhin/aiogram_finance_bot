b76444bfd7f07e91aae39b7c492a4b2e67e6adc3
Vasiliy-Blokhin <Vasiliyblo97@gmail.com>
2024-01-18 02:03:10 +1000

    Modified all project

--- data.py
+++ data.py
@@ -7,9 +7,14 @@ load_dotenv()
 TOKEN = getenv("BOT_TOKEN")
 
 # IMOEX данные
-IMOEX_URL = 'http://iss.moex.com/iss/engines/stock/markets/shares/securities.json?iss.json=extended&iss.meta=off'
+IMOEX_URL = (
+    'http://iss.moex.com/iss/engines/stock/markets/shares/'
+    'securities.json?iss.json=extended&iss.meta=off'
+    )
 
-NEEDFUL = ['SECID', 'SHORTNAME']
+NEEDFUL = [
+    'SECID', 'SHORTNAME', 'PREVPRICE', 'PREVWAPRICE', 'PREVDATE', 'SETTLEDATE'
+]
 
 
 def start_message(name):
@@ -19,8 +24,8 @@ def start_message(name):
     )
     disclaimer_message = (
         'Дисклеймер:\n Результат, который выдает бот может '
-        'быть ошибочным, так как данные берутся со сторонних сервисов (Мосбиржа).\n'
-        'Акции'
+        'быть ошибочным, так как данные берутся со сторонних сервисов '
+        '(Мосбиржа).\n Акции'
         ' подбираются согласно расчетов различных методик, взятых из интернета и на'
         ' основе их выдаются рекомендации.\n Дальнейшие действия на ваш риск. '
         '\n\nНа данный момент бот находится в разработке, так что возможны '
--- module.py
+++ module.py
@@ -19,11 +19,11 @@ handler.setFormatter(formater)
 logger.addHandler(handler)
 
 
-# request 1 for 10000 sec
 class JSONSaveAndRead():
     """ Запись  и чтение полученного json файла."""
     def __init__(self, url) -> None:
         self.url = url
+
     @classmethod
     def get_api_response(self):
         result = []
@@ -34,22 +34,37 @@ class JSONSaveAndRead():
                 if key in NEEDFUL:
                     new_dict[key] = value
             result.append(new_dict)
-        return result
+        return self.test_outdata(result)
 
     def test_indata(indata):
-        try:
+        try:  # Структура входных данных.
             if not isinstance(indata, list):
-                raise TypeError('indata - not list.')
+                raise TypeError('indata - not list')
             if not isinstance(indata[1], dict):
-                raise TypeError('indata[1] - not dict.')
+                raise TypeError('indata[1] - not dict')
             if 'securities' not in indata[1].keys():
-                logger.info(f'---> {indata[1].keys()}')
                 raise ValueError('indata[1] - "securities" not in indata[1]')
             if not isinstance(indata[1]['securities'], list):
-                raise TypeError('indata[1]["securities"] - not list.')
+                raise TypeError('indata[1]["securities"] - not list')
             if not isinstance(indata[1]['securities'][0], dict):
-                raise TypeError('indata[1]["securities"][0] - not dict.')
+                raise TypeError('indata[1]["securities"][0] - not dict')
+            if not set(NEEDFUL).issubset(indata[1]['securities'][0]):
+                raise ValueError('needful not in indata[1]["securities"][0]')
             return indata
+        except Exception as error:
+            logger.error(f'Error ---> {error}')
+            return False
+
+    def test_outdata(outdata):
+        try:  # Структура выходных данных.
+            if not isinstance(outdata, list):
+                raise TypeError('outdata - not list')
+            for element in outdata:
+                if not isinstance(element, dict):
+                    raise TypeError('outdata[element] - not dict')
+                if not set(element.keys()).issubset(NEEDFUL):
+                    raise ValueError('outdata[element] - not in needful')
+            return outdata
         except Exception as error:
             logger.error(f'Error ---> {error}')
             return False
