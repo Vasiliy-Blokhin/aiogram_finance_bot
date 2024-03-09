79ab4fa36854ca7fbb3caa1bb3deb1ec1eb7d1d6
Vasiliy-Blokhin <Vasiliyblo97@gmail.com>
2024-03-09 15:57:10 +1000

    stable version

--- statistic.py
+++ statistic.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 import logging
 from json_worker import JSONSaveAndRead
-from settings import STATISTIC_NEED, STATUS_UP, STATUS_DOWN, handler, FILE_DAILY_STATISTIC
+from settings import STATISTIC_NEED, STATUS_UP, STATUS_DOWN, handler, FILE_DAILY_STATISTIC, COMISSION_COEFF, FILE_UP_ANOMALY
 
 
 # Подключение логгера.
@@ -19,7 +19,6 @@ class StatisticModule(JSONSaveAndRead):
 
     @classmethod
     def data_preparation(self):
-        logger.info('start -> data_preparation')
         result = []
         for jsc in self.json_classes:
             for el in jsc.read_api_request():
@@ -40,26 +39,57 @@ class StatisticModule(JSONSaveAndRead):
     @classmethod
     def get_current_price(self):
         current_data = self.read_api_request()
-        count_positive = 0
+        count_positive_equals = 0
+        count_positive_non_equals = 0
         count_all = 0
-
-        if not current_data:
-            return False
+        count_price_before = 0
+        count_price_after = 0
 
         for el in self.json_all_data:
             for el_cd in current_data:
 
                 if el['SECID'] == el_cd['SECID']:
+                    if el['LAST'] is None:
+                        continue
+
                     el_cd['CUR_PRICE'] = el['LAST']
+
                     if el_cd['STATUS_FILTER'] == STATUS_UP:
                         if el_cd['CUR_PRICE'] >= el_cd['LAST']:
-                            count_positive += 1
+                            count_positive_equals += 1
+                        else:
+                            anomaly_data = self.read_api_request(
+                                FILE_UP_ANOMALY
+                            )
+                            if anomaly_data is None:
+                                anomaly_data = []
+                            anomaly_data.append(el)
+                            self.save_api_request(
+                                file=FILE_UP_ANOMALY,
+                                data=anomaly_data
+                            )
+                        if el_cd['CUR_PRICE'] > el_cd['LAST']:
+                            count_positive_non_equals += 1
+
+                        count_price_before += el_cd['CUR_PRICE']
+                        count_price_after += el_cd['LAST']
+
                     if el_cd['STATUS_FILTER'] == STATUS_DOWN:
                         if el_cd['CUR_PRICE'] <= el_cd['LAST']:
-                            count_positive += 1
+                            count_positive_equals += 1
+                        if el_cd['CUR_PRICE'] < el_cd['LAST']:
+                            count_positive_non_equals += 1
+
                     count_all += 1
 
-        return f"{100 * count_positive/count_all} %"
+        count_positive = (count_positive_equals + count_positive_non_equals) / 2
+        statistic_prcnt = float(100 * count_positive/count_all)
+
+        comission = count_price_after * COMISSION_COEFF
+        potential_profitability = count_price_before - count_price_after - comission
+        if potential_profitability > 0:
+            potential_profitability *= 0.87
+        return [statistic_prcnt, count_price_after, potential_profitability]
 
     @classmethod
     def counting_statistics(self):
